da7cd3c50fc3f082f0358c0d953885dfdd061f96
Onur Karal <santobixua@gmail.com>
2023-09-29 13:35:53 +0300

    Create episodes_screen.dart
    * Episodes screen created.

---
+++ lib/src/presentation/episodes_screen/views/episodes_screen.dart
@@ -0,0 +1,28 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+
+class EpisodeScreen extends StatelessWidget {
+  const EpisodeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const CircularProgressIndicator();
+  }
+}
+
+@RoutePage()
+class _EpisodeScreen extends StatefulWidget {
+  @override
+  State<_EpisodeScreen> createState() => __EpisodeScreen();
+}
+
+class __EpisodeScreen extends State<_EpisodeScreen> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Rick and Morty'), elevation: 10),
+      body: const Center(),
+    );
+  }
+}
