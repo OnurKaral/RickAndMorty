c7852adbe0e76ac687051e607d4be0fc4b1ba649
Onur Karal <santobixua@gmail.com>
2023-10-25 15:22:46 +0300

    directory fixed

--- lib/src/core/router/app_router.dart
+++ lib/src/core/router/app_router.dart
@@ -2,7 +2,7 @@ import 'package:auto_route/auto_route.dart';
 import 'package:rick_and_morty_app/src/presentation/episodes_screen/views/episodes_screen.dart';
 import 'package:rick_and_morty_app/src/presentation/main_screen/views/detail_page.dart';
 import 'package:rick_and_morty_app/src/presentation/main_screen/views/main_screen.dart';
-import 'package:rick_and_morty_app/src/presentation/principal_screen/principal_screen.dart';
+import 'package:rick_and_morty_app/src/presentation/principal_screen/views/principal_screen.dart';
 
 part 'app_router.gr.dart';
 
