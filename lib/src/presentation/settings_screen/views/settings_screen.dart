1785f48b74debe09bff0516a5859f9a83452415f
Onur Karal <santobixua@gmail.com>
2023-10-27 16:29:35 +0300

    settings screen

--- lib/src/presentation/settings_screen/views/settings_screen.dart
+++ lib/src/presentation/settings_screen/views/settings_screen.dart
@@ -1,5 +1,7 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:package_info/package_info.dart';
 
 @RoutePage()
 class SettingsScreen extends StatelessWidget {
@@ -17,8 +19,65 @@ class _SettingsScreen extends StatefulWidget {
 }
 
 class __SettingsScreen extends State<_SettingsScreen> {
+
+  bool isDarkMode = false;
+  String appVersion = "";
+
+  @override
+  void initState() {
+    super.initState();
+    getAppVersion();
+  }
+
+  Future<void> getAppVersion() async {
+    final PackageInfo packageInfo = await PackageInfo.fromPlatform();
+    setState(() {
+      appVersion = packageInfo.version;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Settings'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Text(
+                  'Dark Mode ',
+                  style: TextStyle(fontSize: 18),
+                ),
+                Switch(
+                  value: isDarkMode,
+                  onChanged: (value) {
+                    setState(() {
+                      isDarkMode = value;
+                    });
+                    // Toggle dark mode
+                    if (isDarkMode) {
+                      SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
+                    } else {
+                      SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark);
+                    }
+                  },
+                ),
+              ],
+            ),
+            Text(
+              'App Version: $appVersion',
+              style: const TextStyle(fontSize: 18),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
+
